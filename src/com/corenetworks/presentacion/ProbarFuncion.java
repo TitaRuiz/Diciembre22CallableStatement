3e29059ab74a6aa20fd97d75859a0c0478a13c8f
TitaRuiz <ggp2002es@gmail.com>
2023-12-22 19:09:34 +0100

    19:09

--- src/com/corenetworks/presentacion/ProbarFuncion.java
+++ src/com/corenetworks/presentacion/ProbarFuncion.java
@@ -1,2 +1,19 @@
-package com.corenetworks.presentacion;public class ProbarFuncion {
+package com.corenetworks.presentacion;
+
+import com.corenetworks.persistencia.AccesoFunciones;
+
+import java.sql.SQLException;
+
+public class ProbarFuncion {
+
+    public static void main(String[] args) {
+        AccesoFunciones aF1 = new AccesoFunciones();
+        try {
+            System.out.println("Cantidad en existencia -> " + aF1.probarFuncion());
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.toString());
+        }
+    }
 }
